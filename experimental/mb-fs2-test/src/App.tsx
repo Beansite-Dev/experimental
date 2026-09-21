95064f3e59277cdf9922688925353095d6c8c11c
Tyler <m1dnightgmrofficial@gmail.com>
2026-09-21 01:31:25 -0400

    took browser ocmponent outside of thge app in test for improved efficiency. Also, fixed some linting settings that i quite frankly disagree with and update the version number

--- experimental/mb-fs2-test/src/App.tsx
+++ experimental/mb-fs2-test/src/App.tsx
@@ -1,52 +1,52 @@
 import { useEffect, useState, type ReactElement } from "react";
 import { useFileSystem, type mbfs } from "mb-fs2";
 import "./app.css";
+const Browser=({scope,fsMod}:{
+  scope:mbfs.Directory;
+  fsMod:ReturnType<typeof useFileSystem>[3];
+}):ReactElement=>{
+  const[openFile,setOpenFile]=useState<mbfs.File|null>(null);
+  return(<><div className="browser">
+    <div className="nav">
+      <span>browser</span>
+      <div className="hrv"/>
+      <button onClick={()=>{
+        fsMod.enterParentDirectory();
+      }}>^parent dir</button>
+    </div>
+    {Object.keys(scope.data).map(x=>{
+      const n=scope.data[x],m=n.metadata as {typeof?:{directory?:boolean}}|undefined;
+      return(<div className="child" key={x} onClick={()=>{
+        if(m?.typeof?.directory)fsMod.enterDirectoryFromScope(x);
+        else setOpenFile(n as mbfs.File);
+      }}>
+        <span>{n.name as string}</span>
+        <span>{(m?.typeof?.directory?"[dir]":`.${(scope.data[x]as mbfs.File).type}`)as string}</span>
+        <div className="hrv"/>
+        <span style={{fontSize:".5rem",opacity:".75"}}>uuid:{x}</span>
+      </div>);
+    })}
+  </div><br/>
+  {openFile?<div className="openFile">
+    <div style={{marginBottom:"2px"}} className="nav">
+      {`${openFile.name}.${openFile.type}`}
+      <div className="hrv"/>
+      <button onClick={()=>setOpenFile(null)}>close file</button>
+    </div>
+    {openFile.data as string}
+  </div>:null}<br/></>);
+};
 export const App=({}):ReactElement=>{
   const[
     filesystem,
     scope,
-    directoryTree,
+    _directoryTree,
     fsMod,
   ]=useFileSystem();
   const[tab,setTab]=useState<number>(0);
-  useEffect(()=>{
-    
-  });
-  const Browser=():ReactElement=>{
-    const[openFile,setOpenFile]=useState<mbfs.File|null>(null);
-    return(<><div className="browser">
-      <div className="nav">
-        <span>browser</span>
-        <div className="hrv"/>
-        <button onClick={()=>{
-          fsMod.enterParentDirectory();
-        }}>^parent dir</button>
-      </div>
-      {Object.keys(scope.data).map(x=>{
-        const n=scope.data[x],m=n.metadata as {typeof?:{directory?:boolean}}|undefined;
-        return(<div className="child" key={x} onClick={(e)=>{
-          if(m?.typeof?.directory)fsMod.enterDirectoryFromScope(x);
-          else setOpenFile(n as mbfs.File);
-        }}>
-          <span>{n.name as string}</span>
-          <span>{(m?.typeof?.directory?"[dir]":`.${(scope.data[x]as mbfs.File).type}`)as string}</span>
-          <div className="hrv"/>
-          <span style={{fontSize:".5rem",opacity:".75"}}>uuid:{x}</span>
-        </div>);
-      })}
-    </div><br/>
-    {openFile?<div className="openFile">
-      <div style={{marginBottom:"2px"}} className="nav">
-        {`${openFile.name}.${openFile.type}`}
-        <div className="hrv"/>
-        <button onClick={()=>setOpenFile(null)}>close file</button>
-      </div>
-      {openFile.data as string}
-    </div>:null}<br/></>);
-  }
   return<>
     <h1>mb-fs2 test</h1>
-    <Browser/>
+    <Browser scope={scope} fsMod={fsMod}/>
     <hr/>
     <span style={{minHeight:"1rem !important",display:"block"}}>
       dir tree: {fsMod.getDirTree().join("/")||"(empty...)"}
