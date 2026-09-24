0fd315402236cd0a200fb94fbc77cc75f02bcb42
Tyler <108243683+M1dnight-ofcl@users.noreply.github.com>
2026-09-24 18:12:49 +0000

    more drafting for useShell interpreter hook

--- experimental/mb-shell/src/hook.ts
+++ experimental/mb-shell/src/hook.ts
@@ -25,9 +25,26 @@ export const useShell=():[
     const commands=code.split(/;|\r?\n/);
     for(const command of commands){
       const parseCommand=command.trim().split(" ");
-      switch(parseCommand[0]){
-        
-      }
+      const functionMap:{[key:string]:(dirTree:string[],...args:string[])=>void}={
+        cd:(dirTree:string[],x:string):void=>{
+          // Format of paths:
+          // /root/home/dir1/dir2/file.txt
+          //      /home/dir1/dir2/file.txt
+          //          ~/dir1/dir2/file.txt
+          //          ./dir1/dir2/file.txt
+          // these should be converted to the first kind and
+          // passed as an array to the filesystem hook
+          
+        },
+
+      };
+      const dirTree:string[]=["root"];//temporary, filesys hook should replace
+      setLogs(`> ${command}`);
+      if(functionMap[parseCommand[0]])
+        functionMap[parseCommand[0]](dirTree,...parseCommand.slice(1));
+      else setLogs(
+        `command not found: ${parseCommand[0]}`
+      );
     };
   };
   return[logs,setLogs,interpreter];
